7775df0970e0121d3d3c656e30386ab872417f44
Tiago Ramos Alves <tiagoramosalves@hotmail.com>
2024-04-23 15:10:14 -0300

    First commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::get('/tasks', TaskController::class, 'index');
+Route::resource('/tasks', TaskController::class);
